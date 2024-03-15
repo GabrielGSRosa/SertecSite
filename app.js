174344de0cf738e194732870297c458d80f6ca8f
Gabriel Rosa <88284946+GabrielGSRosa@users.noreply.github.com>
2024-03-15 17:22:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -77,3 +77,10 @@ document.addEventListener('DOMContentLoaded', function () {
     handleScroll();
   });
   
+
+  document.getElementById("whatsappBtn").addEventListener("click", function() {
+    var phoneNumber = "+5511992823569";
+    var message = "Olá, eu vim do site para tirar dúvidas sobre seus serviços.";
+
+    window.open("https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message));
+});
